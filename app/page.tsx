4f7efb08f50a229f970dac903e647f50eae1a935
mrhrifat <mrhrifat383759@gmail.com>
2023-11-08 22:05:50 +0600

    build: homepage

---
+++ app/page.tsx
@@ -0,0 +1,62 @@
+/**
+ * Title: page
+ * Description:
+ * Filename: page.tsx
+ * Path: /app/page.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Created Date: Nov 07, 2023
+ * Last Updated Date: Nov 07, 2023
+ *
+ */
+
+import Image from "next/image";
+import Link from "next/link";
+import { CategroyType } from "./layouts/Categories";
+import { men } from "./utils/data";
+import { stringReform } from "./utils/lib";
+import { getCategory } from "./utils/serverReq";
+
+const Home = async () => {
+  const categories = await getCategory();
+  return (
+    <div className="container mx-auto pt-10 max-sm:px-10">
+      <div className="py-10">
+        <p className="text-center text-3xl font-bold text-primary-900">Dua</p>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+        {categories.map((item: CategroyType) => (
+          <Link
+            key={item.cat_id}
+            href={{
+              pathname: `/duas/${stringReform(item.cat_name_en)}`,
+              query: {
+                cat: item.cat_id,
+              },
+            }}
+          >
+            <div className="flex flex-row gap-x-3 bg-slate-300 items-center p-2 rounded-md">
+              <div className="bg-slate-200 rounded-md">
+                <Image src={men} alt="" className="w-14 h-14 mx-auto p-2" />
+              </div>
+              <div className="flex flex-row w-full justify-between">
+                <div>
+                  <p className="text-primary-900 font-bold">
+                    {item.cat_name_en}
+                  </p>
+                  <small className="text-slate-600 ">
+                    Subcategory: <span>{item.no_of_subcat}</span>
+                  </small>
+                </div>
+                <div>
+                  <p className="text-slate-900 font-bold">{item.no_of_dua}</p>
+                  <small className="text-slate-600">Duas</small>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Home;
