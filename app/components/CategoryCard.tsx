07fdf022fc4c7f0f46fe9a658933e0db3e6f67d2
mrhrifat <mrhrifat383759@gmail.com>
2023-11-08 22:10:54 +0600

    buold: category card & sub categories

---
+++ app/components/CategoryCard.tsx
@@ -0,0 +1,56 @@
+/**
+ * Title: CategoryCard
+ * Description:
+ * Filename: CategoryCard.tsx
+ * Path: /app/components/CategoryCard.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Created Date: Nov 08, 2023
+ * Last Updated Date: Nov 08, 2023
+ *
+ */
+
+import Image from "next/image";
+import Link from "next/link";
+import { CategroyType } from "../layouts/Categories";
+import { men } from "../utils/data";
+import SubCategories from "./SubCategories";
+
+const CategoryCard = ({ item, cat }: { item: CategroyType; cat: string }) => {
+  return (
+    <Link
+      key={item.cat_id}
+      href={{
+        query: {
+          cat: item.cat_id,
+        },
+      }}
+    >
+      <div className="px-3">
+        <div className="flex flex-row gap-x-3 bg-slate-300 items-center p-2 rounded-md">
+          <div className="bg-slate-200 rounded-md">
+            <Image src={men} alt="" className="w-14 h-14 mx-auto p-2" />
+          </div>
+          <div className="flex flex-row w-full justify-between">
+            <div>
+              <p className="text-primary-900 font-bold">{item.cat_name_en}</p>
+              <small className="text-slate-600 ">
+                Subcategory: <span>{item.no_of_subcat}</span>
+              </small>
+            </div>
+            <div>
+              <p className="text-slate-900 font-bold">{item.no_of_dua}</p>
+              <small className="text-slate-600">Duas</small>
+            </div>
+          </div>
+        </div>
+        {cat === item.cat_id.toString() ? (
+          <SubCategories cat_id={item.cat_id} />
+        ) : (
+          <></>
+        )}
+      </div>
+    </Link>
+  );
+};
+
+export default CategoryCard;
---
+++ app/components/SubCategories.tsx
@@ -0,0 +1,41 @@
+/**
+ * Title: SubCategories
+ * Description:
+ * Filename: SubCategories.tsx
+ * Path: /app/components/SubCategories.tsx
+ * Author: Mrh Rifat (Programmer)
+ * Created Date: Nov 08, 2023
+ * Last Updated Date: Nov 08, 2023
+ *
+ */
+
+import { getSubCategory } from "../utils/serverReq";
+
+export type SubCategoryType = {
+  id: number;
+  cat_id: number;
+  subcat_id: number;
+  subcat_name_bn: string;
+  subcat_name_en: string;
+  no_of_dua: number;
+};
+
+const SubCategories = async ({ cat_id }: { cat_id: number }) => {
+  const subCategories = await getSubCategory(cat_id);
+
+  return subCategories?.map((item: SubCategoryType) => (
+    <div
+      className="ml-3 mt-1 border-l-2 border-dotted border-l-primary-900"
+      key={item.id}
+    >
+      <div className="flex flex-col gap-x-3 p-1">
+        <div className="flex flex-row items-center">
+          <span className="bg-primary-900 block h-[7px] w-[7px] rounded-full -translate-x-2"></span>
+          <small className={`text-slate-800`}>{item.subcat_name_en}</small>
+        </div>
+      </div>
+    </div>
+  ));
+};
+
+export default SubCategories;
