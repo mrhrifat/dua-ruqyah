7c712bf6c0f9ae171f26970febfd37ccf87b8172
mrhrifat <mrhrifat383759@gmail.com>
2023-11-09 20:08:10 +0600

    refactor: fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,8 @@
  */
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "./utils/font";
 
 export const metadata: Metadata = {
   title: "Dua Ruqyah",
