b6b377535a91769b5b5e8adc0acbafdad497570f
mrhrifat <mrhrifat383759@gmail.com>
2023-11-09 17:30:45 +0600

    build: section

---
+++ app/components/Section.tsx
@@ -0,0 +1,20 @@
+import { getSubCategory } from "../utils/serverReq";
+import { SubCategoryType } from "./SubCategories";
+
+const Section = async ({ cat, subcat }: { cat: number; subcat: number }) => {
+  const subcategories = await getSubCategory(cat);
+  return subcategories.map(
+    (item: SubCategoryType) =>
+      subcat === item.subcat_id && (
+        <div
+          className="drop-shadow-md bg-white py-3 px-5 rounded-lg mb-5 flex flex-row gap-x-3"
+          key={item.subcat_id}
+        >
+          <p className="text-primary-600">Section: </p>
+          <p className="text-slate-800">{item.subcat_name_en}</p>
+        </div>
+      )
+  );
+};
+
+export default Section;
