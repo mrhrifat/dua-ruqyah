b48d01f05f19d86c464a04ada490f5d6c32db739
mrhrifat <mrhrifat383759@gmail.com>
2023-11-09 20:09:27 +0600

    refactor: header

--- app/duas/layout.tsx
+++ app/duas/layout.tsx
@@ -10,30 +10,38 @@
  */
 
 import { ReactNode } from "react";
+import MenuItem from "../components/MenuItem";
 import Header from "../layouts/Header";
 import Menu from "../layouts/Menu";
 import Settings from "../layouts/Settings";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="container mx-auto 2xl  max-sm:px-5">
-      <div className="grid grid-cols-12 gap-x-8 gap-y-4 pt-6">
-        <div className="col-span-1 hidden xl:block">
-          <Menu />
-        </div>
-        <div className="col-span-12 xl:col-span-11">
-          <header className="max-sm:hidden">
-            <Header />
-          </header>
-          <div className="grid grid-cols-12 justify-between gap-4 pt-4 ">
-            {children}
-            <div className="col-span-3 2xl:block hidden">
-              <Settings />
+    <>
+      <div className="container mx-auto 2xl  max-sm:px-5">
+        <div className="grid grid-cols-12 gap-x-8 gap-y-4 pt-6">
+          <div className="col-span-1 hidden xl:block">
+            <Menu />
+          </div>
+          <div className="col-span-12 xl:col-span-11">
+            <header className="">
+              <Header />
+            </header>
+            <div className="grid grid-cols-12 justify-between gap-4 pt-4 ">
+              {children}
+              <div className="col-span-3 2xl:block hidden">
+                <Settings />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="fixed max-lg:block xl:hidden bottom-0 w-full drop-shadow-md">
+        <div className="flex flex-row gap-x-2 sm:gap-x-6 md:gap-x-14 bg-white py-6 justify-center rounded-tl-[3rem] rounded-tr-[3rem]">
+          <MenuItem />
+        </div>
+      </div>
+    </>
   );
 };
 
